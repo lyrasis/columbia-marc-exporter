44e0cc4ac5a4b3f7da127a1f15a865756c1c6629
Brian Harrington <brian.harrington@lyrasis.org>
2019-06-13 14:55:23 -0400

    Export user-defined string 1 as extra unitid

---
+++ backend/model/columbia-ead-overrides.rb
@@ -0,0 +1,131 @@
+class EADSerializer < ASpaceExport::Serializer
+  serializer_for :ead
+
+  require 'pp'
+
+  
+  def stream(data)
+    @stream_handler = ASpaceExport::StreamHandler.new
+    @fragments = ASpaceExport::RawXMLHandler.new
+    @include_unpublished = data.include_unpublished?
+    @include_daos = data.include_daos?
+    @use_numbered_c_tags = data.use_numbered_c_tags?
+    @id_prefix = I18n.t('archival_object.ref_id_export_prefix', :default => 'aspace_')
+
+    doc = Nokogiri::XML::Builder.new(:encoding => "UTF-8") do |xml|
+      begin
+
+      ead_attributes = {
+        'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+        'xsi:schemaLocation' => 'urn:isbn:1-931666-22-9 http://www.loc.gov/ead/ead.xsd',
+        'xmlns:xlink' => 'http://www.w3.org/1999/xlink'
+      }
+
+      if data.publish === false
+        ead_attributes['audience'] = 'internal'
+      end
+
+      xml.ead( ead_attributes ) {
+
+        xml.text (
+          @stream_handler.buffer { |xml, new_fragments|
+            serialize_eadheader(data, xml, new_fragments)
+          })
+
+        atts = {:level => data.level, :otherlevel => data.other_level}
+        atts.reject! {|k, v| v.nil?}
+
+        xml.archdesc(atts) {
+
+          xml.did {
+
+
+            if (val = data.language)
+              xml.langmaterial {
+                xml.language(:langcode => val) {
+                  xml.text I18n.t("enumerations.language_iso639_2.#{val}", :default => val)
+                }
+              }
+            end
+
+            if (val = data.repo.name)
+              xml.repository {
+                xml.corpname { sanitize_mixed_content(val, xml, @fragments) }
+              }
+            end
+
+            if (val = data.title)
+              xml.unittitle  {   sanitize_mixed_content(val, xml, @fragments) }
+            end
+
+            serialize_origination(data, xml, @fragments)
+
+            xml.unitid (0..3).map{|i| data.send("id_#{i}")}.compact.join('.')
+            xml.unitid data.user_defined['string_1']
+
+            if @include_unpublished
+              data.external_ids.each do |exid|
+                xml.unitid  ({ "audience" => "internal", "type" => exid['source'], "identifier" => exid['external_id']}) { xml.text exid['external_id']}
+              end
+            end
+
+            serialize_extents(data, xml, @fragments)
+
+            serialize_dates(data, xml, @fragments)
+
+            serialize_did_notes(data, xml, @fragments)
+
+            data.instances_with_sub_containers.each do |instance|
+              serialize_container(instance, xml, @fragments)
+            end
+
+            EADSerializer.run_serialize_step(data, xml, @fragments, :did)
+
+          }# </did>
+
+          data.digital_objects.each do |dob|
+            serialize_digital_object(dob, xml, @fragments)
+          end
+
+          serialize_nondid_notes(data, xml, @fragments)
+
+          serialize_bibliographies(data, xml, @fragments)
+
+          serialize_indexes(data, xml, @fragments)
+
+          serialize_controlaccess(data, xml, @fragments)
+
+          EADSerializer.run_serialize_step(data, xml, @fragments, :archdesc)
+
+          xml.dsc {
+
+            data.children_indexes.each do |i|
+              xml.text(
+                       @stream_handler.buffer {|xml, new_fragments|
+                         serialize_child(data.get_child(i), xml, new_fragments)
+                       }
+                       )
+            end
+          }
+        }
+      }
+
+    rescue => e
+      xml.text  "ASPACE EXPORT ERROR : YOU HAVE A PROBLEM WITH YOUR EXPORT OF YOUR RESOURCE. THE FOLLOWING INFORMATION MAY HELP:\n
+                MESSAGE: #{e.message.inspect}  \n
+                TRACE: #{e.backtrace.inspect} \n "
+    end
+
+
+
+    end
+    doc.doc.root.add_namespace nil, 'urn:isbn:1-931666-22-9'
+
+    Enumerator.new do |y|
+      @stream_handler.stream_out(doc, @fragments, y)
+    end
+
+
+  end
+
+end
