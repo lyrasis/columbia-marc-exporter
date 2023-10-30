a704d648d5c8a27979892e684d4202f9acf6694b
Brian Harrington <brian.harrington@lyrasis.org>
2023-10-30 14:14:56 -0400

    Change repo code based on udf string_1

---
+++ backend/model/columbia-ead-model-overrides.rb
@@ -0,0 +1,14 @@
+class EADModel < ASpaceExport::ExportModel
+  model_for :ead
+
+  include ASpaceExport::ArchivalObjectDescriptionHelpers
+  include ASpaceExport::LazyChildEnumerations
+
+  def mainagencycode
+    if user_defined['string_1'].start_with?("UA")
+      repo['org_code'] = "NNC-UA"
+    end
+    @mainagencycode ||= repo.country && repo.org_code ? [repo.country, repo.org_code].join('-') : nil
+    @mainagencycode
+  end
+end
