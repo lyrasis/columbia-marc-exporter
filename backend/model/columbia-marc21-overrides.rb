19da1c55faa285e153560ddaddb5d479d342cd5f
Brian Harrington <brian.harrington@lyrasis.org>
2024-01-29 15:58:27 -0500

    Remove old code for separating repeated fields

--- backend/model/columbia-marc21-overrides.rb
+++ backend/model/columbia-marc21-overrides.rb
@@ -31,28 +31,6 @@ class MARCModel < ASpaceExport::ExportModel
     :ead_location => :handle_ead_loc
   }
 
-  #Picked from new release, but added 035
-#  def df(*args)
-#    if @datafields.has_key?(args.to_s)
-#      # Manny Rodriguez: 3/16/18
-#      # Bugfix for ANW-146
-#      # Separate creators should go in multiple 700 fields in the output MARCXML file. This is not happening because the different 700 fields are getting mashed in under the same key in the hash below, instead of having a new hash entry created.
-#      # So, we'll get around that by using a different hash key if the code is 700.
-#      # based on MARCModel#datafields, it looks like the hash keys are thrown away outside of this class, so we can use anything as a key.
-#      # At the moment, we don't want to change this behavior too much in case something somewhere else is relying on the original behavior.
-#
-#     if(args[0] == "700" || args[0] == "710" || args[0] == "035" || args[0] == "506")
-#       @datafields[rand(10000)] = @@datafield.new(*args)
-#     else
-#       @datafields[args.to_s]
-#     end
-#    else
-#
-#      @datafields[args.to_s] = @@datafield.new(*args)
-#      @datafields[args.to_s]
-#    end
-#  end
-
   #Add an 035 based on string + id0
   def handle_id(*ids)
     ids.reject!{|i| i.nil? || i.empty?}
